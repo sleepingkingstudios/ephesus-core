41fcc23d57d012e7006446b75fc7bb6af6d21cb6
Rob Smith <merlin@sleepingkingstudios.com>
2018-10-05 15:58:42 -0400

    Implement Action::argument, ::keyword.

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -5,10 +5,37 @@
 ## Actions
 
 - DSL: set options for Controller#available_actions (see below)
+- ::properties method:
+  - returns Hash of action info, see #available_actions
+- ::signature method:
+  - returns an Action::Signature for the Action, see below
+
+## Action::Result
+
+- Custom subclass of Cuprum::Result.
+- Action generates with #build_result method.
+- Has additional properties:
+  - action_name
+  - arguments
+  - keywords
+
+### Action::Signature
+
+- constructor takes an Action
+- #match(arguments, keywords):
+
+  returns [success(Boolean), error_result(Action::Result, nil)]
 
 ## Applications
 
 ## Controllers
+
+- ::action:
+
+  Set additional metadata
+  - :name
+  - :properties, from action_class::properties
+  - :signature, from action_class::signature
 
 - #available_actions :
   ```
@@ -51,6 +78,10 @@
   }
   ```
 
+- #execute_action:
+  - catch (pre-check?) argument errors, respond with failing Result
+  - see Action::Signature above
+
 - conditional actions with method names:
 
   action :request_clearance, RequestClearance, if: :can_request_clearance?
@@ -59,6 +90,15 @@
 
   alias_action :do_trick, as: :do_a_trick
   alias_action :dance, as: %i[shake rattle roll]
+
+## Events
+
+- StateUpdateEvent:
+  - used for handling output
+  - CANNOT BE USED IN A REDUCER
+  - #parent_event
+  - #previous_state
+  - #current_state
 
 ## Ephesus::RSpec
 
---
+++ spec/ephesus/core/actions/dsl_spec.rb
@@ -0,0 +1,311 @@
+# frozen_string_literal: true
+
+require 'ephesus/core/action'
+
+RSpec.describe Ephesus::Core::Actions::Dsl do
+  shared_context 'when the action defines an argument' do
+    before(:example) do
+      action_class.send :argument, :do_something
+
+      properties[:arguments] << {
+        name:     :do_something,
+        required: true
+      }
+    end
+  end
+
+  shared_context 'when the action defines many arguments' do
+    before(:example) do
+      action_class.send :argument, :argument_one
+      action_class.send :argument, :argument_two,   required: true
+      action_class.send :argument, :argument_three, required: false
+
+      properties[:arguments] << {
+        name:     :argument_one,
+        required: true
+      }
+
+      properties[:arguments] << {
+        name:     :argument_two,
+        required: true
+      }
+
+      properties[:arguments] << {
+        name:     :argument_three,
+        required: false
+      }
+    end
+  end
+
+  shared_context 'when the action defines a keyword' do
+    before(:example) do
+      action_class.send :keyword, :with_option
+
+      properties[:keywords][:with_option] = {
+        name:     :with_option,
+        required: false
+      }
+    end
+  end
+
+  shared_context 'when the action defines many keywords' do
+    before(:example) do
+      action_class.send :keyword, :keyword_one
+      action_class.send :keyword, :keyword_two,   required: false
+      action_class.send :keyword, :keyword_three, required: true
+
+      properties[:keywords][:keyword_one] = {
+        name:     :keyword_one,
+        required: false
+      }
+
+      properties[:keywords][:keyword_two] = {
+        name:     :keyword_two,
+        required: false
+      }
+
+      properties[:keywords][:keyword_three] = {
+        name:     :keyword_three,
+        required: true
+      }
+    end
+  end
+
+  let(:action_class) { Class.new(Ephesus::Core::Action) }
+  let(:properties) do
+    {
+      arguments: [],
+      keywords:  {}
+    }
+  end
+
+  it { expect(action_class).to be < described_class }
+
+  describe '::argument' do
+    let(:name) { :spell }
+    let(:expected) do
+      {
+        name:     name.intern,
+        required: true
+      }
+    end
+
+    it { expect(action_class).not_to respond_to(:argument) }
+
+    it 'should define the private method' do
+      expect(action_class)
+        .to respond_to(:argument, true)
+        .with(1).argument.and_keywords(:required)
+    end
+
+    it 'should add the argument to the properties' do
+      expect { action_class.send(:argument, name) }
+        .to change { action_class.properties[:arguments] }
+        .to include expected
+    end
+
+    describe 'with a String' do
+      let(:name) { 'spell' }
+
+      it 'should add the argument to the properties' do
+        expect { action_class.send(:argument, name) }
+          .to change { action_class.properties[:arguments] }
+          .to include expected
+      end
+    end
+
+    describe 'with a String in camel case' do
+      let(:name) { 'MysticIncantation' }
+      let(:expected) do
+        super().tap do |hsh|
+          hsh[:name] = :mystic_incantation
+        end
+      end
+
+      it 'should add the argument to the properties' do
+        expect { action_class.send(:argument, name) }
+          .to change { action_class.properties[:arguments] }
+          .to include expected
+      end
+    end
+
+    describe 'with a String with whitespace' do
+      let(:name) { 'eldritch invocation' }
+      let(:expected) do
+        super().tap do |hsh|
+          hsh[:name] = :eldritch_invocation
+        end
+      end
+
+      it 'should add the argument to the properties' do
+        expect { action_class.send(:argument, name) }
+          .to change { action_class.properties[:arguments] }
+          .to include expected
+      end
+    end
+
+    describe 'with required: false' do
+      let(:expected) { super().merge required: false }
+
+      it 'should add the argument to the properties' do
+        expect { action_class.send(:argument, name, required: false) }
+          .to change { action_class.properties[:arguments] }
+          .to include expected
+      end
+    end
+
+    describe 'with required: true' do
+      it 'should add the argument to the properties' do
+        expect { action_class.send(:argument, name, required: true) }
+          .to change { action_class.properties[:arguments] }
+          .to include expected
+      end
+    end
+
+    wrap_context 'when the action defines an argument' do
+      it 'should add the argument to the properties' do
+        expect { action_class.send(:argument, name) }
+          .to change { action_class.properties[:arguments] }
+          .to include expected
+      end
+    end
+
+    wrap_context 'when the action defines many arguments' do
+      it 'should add the argument to the properties' do
+        expect { action_class.send(:argument, name) }
+          .to change { action_class.properties[:arguments] }
+          .to include expected
+      end
+    end
+  end
+
+  describe '::keyword' do
+    let(:name) { :target }
+    let(:type) { String }
+    let(:expected) do
+      {
+        name:     name.intern,
+        required: false
+      }
+    end
+
+    it { expect(action_class).not_to respond_to(:keyword) }
+
+    it 'should define the private method' do
+      expect(action_class)
+        .to respond_to(:keyword, true)
+        .with(1).argument.and_keywords(:required)
+    end
+
+    it 'should add the keyword to the properties' do
+      expect { action_class.send(:keyword, name) }
+        .to change { action_class.properties[:keywords] }
+        .to include(expected[:name] => expected)
+    end
+
+    describe 'with a String' do
+      let(:name) { 'target' }
+
+      it 'should add the keyword to the properties' do
+        expect { action_class.send(:keyword, name) }
+          .to change { action_class.properties[:keywords] }
+          .to include(expected[:name] => expected)
+      end
+    end
+
+    describe 'with a String in camel case' do
+      let(:name) { 'AimAt' }
+      let(:expected) do
+        super().tap do |hsh|
+          hsh[:name] = :aim_at
+        end
+      end
+
+      it 'should add the keyword to the properties' do
+        expect { action_class.send(:keyword, name) }
+          .to change { action_class.properties[:keywords] }
+          .to include(expected[:name] => expected)
+      end
+    end
+
+    describe 'with a String with whitespace' do
+      let(:name) { 'thing wot we want to hit' }
+      let(:expected) do
+        super().tap do |hsh|
+          hsh[:name] = :thing_wot_we_want_to_hit
+        end
+      end
+
+      it 'should add the keyword to the properties' do
+        expect { action_class.send(:keyword, name) }
+          .to change { action_class.properties[:keywords] }
+          .to include(expected[:name] => expected)
+      end
+    end
+
+    describe 'with required: false' do
+      it 'should add the keyword to the properties' do
+        expect { action_class.send(:keyword, name, required: false) }
+          .to change { action_class.properties[:keywords] }
+          .to include(expected[:name] => expected)
+      end
+    end
+
+    describe 'with required: true' do
+      let(:expected) { super().merge required: true }
+
+      it 'should add the keyword to the properties' do
+        expect { action_class.send(:keyword, name, required: true) }
+          .to change { action_class.properties[:keywords] }
+          .to include(expected[:name] => expected)
+      end
+    end
+
+    wrap_context 'when the action defines a keyword' do
+      it 'should add the keyword to the properties' do
+        expect { action_class.send(:keyword, name) }
+          .to change { action_class.properties[:keywords] }
+          .to include(expected[:name] => expected)
+      end
+    end
+
+    wrap_context 'when the action defines many keywords' do
+      it 'should add the keyword to the properties' do
+        expect { action_class.send(:keyword, name) }
+          .to change { action_class.properties[:keywords] }
+          .to include(expected[:name] => expected)
+      end
+    end
+  end
+
+  describe '::properties' do
+    let(:expected) { properties }
+
+    it { expect(action_class).to have_reader(:properties) }
+
+    it { expect(action_class.properties).to be == expected }
+
+    wrap_context 'when the action defines an argument' do
+      it { expect(action_class.properties).to be == expected }
+    end
+
+    wrap_context 'when the action defines many arguments' do
+      it { expect(action_class.properties).to be == expected }
+    end
+
+    wrap_context 'when the action defines a keyword' do
+      it { expect(action_class.properties).to be == expected }
+    end
+
+    wrap_context 'when the action defines many keywords' do
+      it { expect(action_class.properties).to be == expected }
+    end
+
+    context 'when the action defines many arguments and keywords' do
+      include_context 'when the action defines many arguments'
+      include_context 'when the action defines many keywords'
+
+      it { expect(action_class.properties).to be == expected }
+    end
+  end
+end
