a2821f9e2b52d52f403a01b92f783409709eae94
Rob Smith <merlin@sleepingkingstudios.com>
2018-11-28 15:12:15 -0500

    Implement Commands::BuiltIn::CommandData.

---
+++ spec/ephesus/core/commands/built_in/command_data_spec.rb
@@ -0,0 +1,157 @@
+# frozen_string_literal: true
+
+require 'ephesus/core/commands/built_in/command_data'
+
+RSpec.describe Ephesus::Core::Commands::BuiltIn::CommandData do
+  subject(:instance) { described_class.new(available_commands) }
+
+  let(:available_commands) { {} }
+
+  describe '::new' do
+    it { expect(described_class).to be_constructible.with(1).argument }
+  end
+
+  describe '::properties' do
+    let(:arguments) do
+      [
+        {
+          description: 'The name of the command to query.',
+          name:        :command,
+          required:    true
+        }
+      ]
+    end
+    let(:expected) do
+      {
+        arguments:        arguments,
+        description:      'Provides information about the requested command.',
+        examples:         [],
+        full_description: nil,
+        keywords:         {}
+      }
+    end
+
+    it { expect(described_class.properties).to be == expected }
+  end
+
+  describe '::COMMAND_NOT_FOUND_ERROR' do
+    include_examples 'should define immutable constant',
+      :COMMAND_NOT_FOUND_ERROR,
+      'ephesus.core.commands.command_data.command_not_found'
+  end
+
+  describe '#available_commands' do
+    include_examples 'should have reader',
+      :available_commands,
+      -> { available_commands }
+  end
+
+  describe '#call' do
+    it { expect(instance).to respond_to(:call).with(1).argument }
+
+    describe 'with nil' do
+      let(:expected_error) do
+        {
+          type:   described_class::COMMAND_NOT_FOUND_ERROR,
+          params: { command: nil }
+        }
+      end
+
+      it { expect(instance.call(nil).value).to be nil }
+
+      it { expect(instance.call(nil).success?).to be false }
+
+      it { expect(instance.call(nil).errors).to include expected_error }
+    end
+
+    describe 'with an invalid command name' do
+      let(:expected_error) do
+        {
+          type:   described_class::COMMAND_NOT_FOUND_ERROR,
+          params: { command: 'do nothing' }
+        }
+      end
+
+      it { expect(instance.call('do nothing').value).to be nil }
+
+      it { expect(instance.call('do nothing').success?).to be false }
+
+      it 'should set the errors' do
+        expect(instance.call('do nothing').errors).to include expected_error
+      end
+    end
+
+    context 'when there are many available commands' do
+      let(:available_commands) do
+        {
+          do_something: {
+            aliases:          ['do something', 'do the mario'],
+            arguments:        [],
+            description:      'Does something, probably.',
+            examples:         [],
+            full_description: '',
+            keywords:         {}
+          }
+        }
+      end
+
+      # rubocop:disable RSpec/NestedGroups
+      describe 'with nil' do
+        let(:expected_error) do
+          {
+            type:   described_class::COMMAND_NOT_FOUND_ERROR,
+            params: { command: nil }
+          }
+        end
+
+        it { expect(instance.call(nil).value).to be nil }
+
+        it { expect(instance.call(nil).success?).to be false }
+
+        it { expect(instance.call(nil).errors).to include expected_error }
+      end
+
+      describe 'with an invalid command name' do
+        let(:expected_error) do
+          {
+            type:   described_class::COMMAND_NOT_FOUND_ERROR,
+            params: { command: 'do nothing' }
+          }
+        end
+
+        it { expect(instance.call('do nothing').value).to be nil }
+
+        it { expect(instance.call('do nothing').success?).to be false }
+
+        it 'should set the errors' do
+          expect(instance.call('do nothing').errors).to include expected_error
+        end
+      end
+
+      describe 'with a valid command name as a symbol' do
+        let(:expected) { available_commands.fetch(:do_something) }
+
+        it { expect(instance.call(:do_something).value).to be == expected }
+
+        it { expect(instance.call(:do_something).success?).to be true }
+      end
+
+      describe 'with a valid command name as a string' do
+        let(:expected) { available_commands.fetch(:do_something) }
+
+        it { expect(instance.call('do something').value).to be == expected }
+
+        it { expect(instance.call('do something').success?).to be true }
+      end
+
+      describe 'with a valid command alias' do
+        let(:expected) { available_commands.fetch(:do_something) }
+
+        it { expect(instance.call('do the Mario').value).to be == expected }
+
+        it { expect(instance.call('do the Mario').success?).to be true }
+      end
+      # rubocop:enable RSpec/NestedGroups
+    end
+  end
+end
